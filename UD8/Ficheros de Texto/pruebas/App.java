5eb0709e9f3e4cfbfd1a4d74aa0a14a8672bd64a
daw1 - Carballo Puebla <A23OscarCP@sanclemente.local>
2024-03-01 14:25:15 +0100

    1/3/2024

--- UD8/Ficheros de Texto/pruebas/App.java
+++ UD8/Ficheros de Texto/pruebas/App.java
@@ -3,17 +3,22 @@ package pruebas;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.*;
 
-public class App{
-  public static void main(String[] args) throws IOException{
+public class App {
+
+  public static void main(String[] args)
+    throws FileNotFoundException, IOException {
     File doc = new File(".\\pruebas\\Demo.txt");
-    FileReader freader = new FileReader(doc);
-
-    char[] i = new char[100];
-    freader.read(i);
-    for(char j : i){
-      System.out.print(j);
+    //Uso do try with resources, non fai falta usar o close
+    try (
+      BufferedReader bufferedReader = new BufferedReader(new FileReader(doc))
+    ) {
+      String line = bufferedReader.readLine();
+      while (line != null) {
+        System.out.println(line);
+        line = bufferedReader.readLine();
+      }
     }
-    freader.close();
   }
 }
